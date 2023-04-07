60659a79d7f0cf32272ac921cf2b7ab51950bba7
Angus Chang <changangus2@gmail.com>
2023-04-07 10:59:49 -0400

    feat: rename get_pulls to get pr and add filter by reviewer function

--- lua/github_pulls/api.lua
+++ lua/github_pulls/api.lua
@@ -3,14 +3,16 @@ local decode = vim.fn.json_decode
 
 local M = {}
 
+M.username = "changangus"
+
 local headers = {
   ["Accept"] = "application/json",
   ["Authorization"] = "Bearer <TOKEN_GOES_HERE>",
   ["X-GitHub-Api-Version"] = "2022-11-28"
 }
 
-M.get_pulls = function()
   local response = curl.get({ url = 'https://api.github.com/repos/<username>/<repo>/pulls', headers = headers })
+M.get_prs = function()
 
   return decode(response.body)
 end
@@ -19,14 +21,56 @@ M.translate_data = function(pulls)
   local data = {}
 
   for _, pull in ipairs(pulls) do
-    table.insert(data, {
-      title = pull.title,
-      number = pull.number,
-      html_url = pull.html_url,
-    })
+      table.insert(data, {
+        title = pull.title,
+        number = pull.number,
+        html_url = pull.html_url,
+        reviewers = pull.requested_reviewers,
+        branch_name = pull.head.ref,
+      })
   end
 
   return data
 end
+
+M.get_prs_by_user = function()
+  local pulls = M.get_prs()
+  local data = {}
+
+  for _, pull in ipairs(pulls) do
+    if pull.user.login == M.username then
+      table.insert(data, {
+        title = pull.title,
+        number = pull.number,
+        html_url = pull.html_url,
+        reviewers = pull.requested_reviewers,
+        branch_name = pull.head.ref,
+      })
+    end
+  end
+
+  return data
+end
+
+M.get_reviews_by_user = function()
+  local pulls = M.get_prs()
+  local data = {}
+
+  for _, pull in ipairs(pulls) do
+    for _, reviewer in ipairs(pull.requested_reviewers) do
+      if reviewer.login == M.username then
+        table.insert(data, {
+          title = pull.title,
+          number = pull.number,
+          html_url = pull.html_url,
+          branch_name = pull.head.ref,
+        })
+      end
+    end
+  end
+
+  return data
+end
+
 
 return M
