945b53eded718d5ec0c717604bd1a9beb9f9ecea
Angus Chang <changangus2@gmail.com>
2023-04-22 13:42:53 -0400

    refactor: reorganize file, change global vars to local

--- lua/github_pulls/ui.lua
+++ lua/github_pulls/ui.lua
@@ -17,12 +17,28 @@ local get_branch_name = function(pulls)
   return branch_names
 end
 
-M.prs = api.get_prs_by_user()
-M.pr_branch_names = get_branch_name(M.prs)
-M.pr_urls = get_urls(M.prs)
-M.reviews = api.get_reviews_by_user()
-M.review_urls = get_urls(M.reviews)
-M.review_branch_names = get_branch_name(M.reviews)
+local prs = api.get_prs_by_user()
+local pr_branch_names = get_branch_name(prs)
+local pr_urls = get_urls(prs)
+local reviews = api.get_reviews_by_user()
+local review_urls = get_urls(reviews)
+local review_branch_names = get_branch_name(reviews)
+
+local open_url = function(url)
+  local cmd = ""
+  if vim.fn.has("mac") == 1 then
+    cmd = "open"
+  elseif vim.fn.has("unix") == 1 then
+    cmd = "xdg-open"
+  elseif vim.fn.has("win32") == 1 then
+    cmd = "start"
+  else
+    print("Unsupported platform")
+    return
+  end
+  vim.fn.jobstart({ cmd, url })
+end
+
 
 local function create_pr_window()
   local width = M.config.width
@@ -66,7 +82,7 @@ M.toggle_pr_menu = function()
   Prs_win_id = win_info.win_id
   Prs_bufh = win_info.bufnr
 
-  for i, pull in ipairs(M.prs) do
+  for i, pull in ipairs(prs) do
     vim.api.nvim_buf_set_lines(Prs_bufh, i, i, false, { pull.title })
   end
   vim.api.nvim_buf_set_keymap(Prs_bufh, "n", "<esc>", ":q<CR>", { noremap = true, silent = true })
@@ -77,31 +93,16 @@ M.toggle_pr_menu = function()
   vim.api.nvim_buf_set_option(Prs_bufh, "modifiable", false)
 end
 
-M.open_url = function(url)
-  local cmd = ""
-  if vim.fn.has("mac") == 1 then
-    cmd = "open"
-  elseif vim.fn.has("unix") == 1 then
-    cmd = "xdg-open"
-  elseif vim.fn.has("win32") == 1 then
-    cmd = "start"
-  else
-    print("Unsupported platform")
-    return
-  end
-  vim.fn.jobstart({ cmd, url })
-end
-
 M.open_pull = function()
   local line = vim.api.nvim_win_get_cursor(0)[1]
-  local url = M.pr_urls[line - 1]
-  M.open_url(url)
+  local url = pr_urls[line - 1]
+  open_url(url)
   vim.api.nvim_win_close(Prs_win_id, true)
 end
 
 M.pr_checkout_branch = function()
   local line = vim.api.nvim_win_get_cursor(0)[1]
-  local branch_name = M.pr_branch_names[line - 1]
+  local branch_name = pr_branch_names[line - 1]
 
   -- execute the Git command and capture the stderr output
   local git_cmd = "git checkout " .. branch_name .. " 2> /tmp/git_error.log"
@@ -110,47 +111,6 @@ M.pr_checkout_branch = function()
   if status == 0 then
     -- checkout succeeded, close the window
     vim.api.nvim_win_close(Prs_win_id, true)
-  else
-    -- checkout failed, set buffer lines to the error message
-    local error_file = io.open("/tmp/git_error.log", "r")
-    if error_file == nil then
-      print("Error opening /tmp/git_error.log")
-      return
-    end
-    local error_message = error_file:read("*all")
-    error_file:close()
-
-    error_message = string.gsub(error_message, "\n", " ")
-    -- replace multiple spaces with a single space
-    error_message = string.gsub(error_message, "%s+", " ")
-
-    -- set the color to red
-    local red = "ErrorMsg"
-
-    -- print a message in red
-    vim.api.nvim_echo({ { error_message, red } }, true, {})
-  end
-end
-
-
-M.open_review = function()
-  local line = vim.api.nvim_win_get_cursor(0)[1]
-  local url = M.review_urls[line - 1]
-  M.open_url(url)
-  vim.api.nvim_win_close(Reviews_win_id, true)
-end
-
-M.review_checkout_branch = function()
-  local line = vim.api.nvim_win_get_cursor(0)[1]
-  local branch_name = M.review_branch_names[line - 1]
-
-  -- execute the Git command and capture the stderr output
-  local git_cmd = "git checkout " .. branch_name .. " 2> /tmp/git_error.log"
-  local status = os.execute(git_cmd)
-
-  if status == 0 then
-    -- checkout succeeded, close the window
-    vim.api.nvim_win_close(Reviews_win_id, true)
   else
     -- checkout failed, set buffer lines to the error message
     local error_file = io.open("/tmp/git_error.log", "r")
@@ -214,13 +174,54 @@ M.toggle_reviews_menu = function()
   Reviews_win_id = win_info.win_id
   Reviews_bufh = win_info.bufnr
 
-  for i, review in ipairs(M.reviews) do
+  for i, review in ipairs(reviews) do
     vim.api.nvim_buf_set_lines(Reviews_bufh, i, i, false, { review.title })
   end
   vim.api.nvim_buf_set_keymap(Reviews_bufh, "n", "<esc>", ":q<CR>", { noremap = true, silent = true })
   vim.api.nvim_buf_set_keymap(Reviews_bufh, "n", "<cr>", ":lua require('github_pulls.ui').open_review()<CR>",
     { noremap = true, silent = true })
   vim.api.nvim_buf_set_option(Reviews_bufh, "modifiable", false)
+end
+
+M.open_review = function()
+  local line = vim.api.nvim_win_get_cursor(0)[1]
+  local url = review_urls[line - 1]
+  open_url(url)
+  vim.api.nvim_win_close(Reviews_win_id, true)
+end
+
+
+M.review_checkout_branch = function()
+  local line = vim.api.nvim_win_get_cursor(0)[1]
+  local branch_name = review_branch_names[line - 1]
+
+  -- execute the Git command and capture the stderr output
+  local git_cmd = "git checkout " .. branch_name .. " 2> /tmp/git_error.log"
+  local status = os.execute(git_cmd)
+
+  if status == 0 then
+    -- checkout succeeded, close the window
+    vim.api.nvim_win_close(Reviews_win_id, true)
+  else
+    -- checkout failed, set buffer lines to the error message
+    local error_file = io.open("/tmp/git_error.log", "r")
+    if error_file == nil then
+      print("Error opening /tmp/git_error.log")
+      return
+    end
+    local error_message = error_file:read("*all")
+    error_file:close()
+
+    error_message = string.gsub(error_message, "\n", " ")
+    -- replace multiple spaces with a single space
+    error_message = string.gsub(error_message, "%s+", " ")
+
+    -- set the color to red
+    local red = "ErrorMsg"
+
+    -- print a message in red
+    vim.api.nvim_echo({ { error_message, red } }, true, {})
+  end
 end
 
 return M
